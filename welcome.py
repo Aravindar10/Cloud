e02f7ff49e2d90d4e6b541a19ddd833e268b6966
Aravindar10 <149231058+Aravindar10@users.noreply.github.com>
2024-09-02 16:48:24 +0530

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to my web application!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
